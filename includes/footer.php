59be32bf83487d1f84092b7ed8ff98ec62f747fc
Anita Nandi <anita.k.nandi@gmail.com>
2017-08-07 22:09:28 +0100

    Edit footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,26 +2,26 @@
                 <div class="container">
                         <div class="row">
                                 <div class="col-lg-4">
-                                        <h4>My Bunker</h4>
+                                        <h4>About this site</h4>
                                         <p>
-                                                Some Address 987,<br/>
-                                                +34 9054 5455, <br/>
-                                                Madrid, Spain.
+						Scientific research for anyone and everyone
                                         </p>
                                 </div><!-- /col-lg-4 -->
 
                                 <div class="col-lg-4">
+<?php /*
                                         <h4>My Links</h4>
                                         <p>
                                                 <a href="#">Dribbble</a><br/>
                                                 <a href="#">Twitter</a><br/>
                                                 <a href="#">Facebook</a>
                                         </p>
+*/?>
                                 </div><!-- /col-lg-4 -->
 
                                 <div class="col-lg-4">
-                                        <h4>About Anita</h4>
-                                        <p>This cute theme was created to showcase your work in a simple way. Use it wisely.</p>
+                                        <h4>About Me</h4>
+                                        <p>I am a particle physics research student who enjoys writing about science</p>
                                 </div><!-- /col-lg-4 -->
 
                         </div>
