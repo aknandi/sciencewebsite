bbfd0853c8bd8b8208819ffdf9d88201affd71c8
Anita Nandi <anita.k.nandi@gmail.com>
2017-08-08 14:59:10 +0100

    Add about page and change title

---
+++ about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php include "./includes/preamble.php"; ?>
+
+  <body>
+
+  <?php include "./includes/header.php"; ?>
+
+       <div class="container pt">
+                <div class="row mt">
+                        <div class="col-lg-6 col-lg-offset-3 centered">
+                                <h3>ABOUT</h3>
+                                <hr>
+                                <p>
+There are so many fasinating things that we know about our world from scientific research: how mountains form, what the sun is made of, why the sky is blue and many others. But there are still many things we don't know. What makes up 95% of our universe? What is consciousness? Why do we sleep? How does a bicycle work? Every day scientists continue to find new exciting developments about the universe we live in at all scales from sub-atmoic particles to galaxies. <br/><br/>
+
+I am a particle physics research student with a strong interest in science communication. This site is based on my belief that it is very important for anyone to be able to read about to the latest interesting developments in science. More openess and communication between scientists and the public enourages the development of trust in science and promotes evidence-based reasoning.<br/><br/>
+
+Hope you find it interesting!
+</p>
+                        </div>
+                </div>
+       </div>
+
+  <?php include "./includes/footer.php"; ?>
+
+
+    <!-- Bootstrap core JavaScript                                                                                                                                                                           
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -6,11 +6,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="./">Science Website</a>
+          <a class="navbar-brand" href="./">Science For Everyone</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
             <li><a href="./">Home</a></li>
+            <li><a href="about.php">About</a></li>
             <li><a href="contact.php">Contact</a></li>
           </ul>
         </div>
