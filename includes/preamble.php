fa620d009ad5aad64b66af047f581558ed0561ac
Anita Nandi <anita.k.nandi@gmail.com>
2017-11-14 00:23:08 +0000

    Made js files local

--- includes/preamble.php
+++ includes/preamble.php
@@ -8,10 +8,10 @@
     <link rel="icon" href="<?=get_url('includes/favicon.jpg')?>">
     <link href="<?=get_url('assets/css/bootstrap.css')?>" rel="stylesheet">
     <link href="<?=get_url('assets/css/main.css')?>" rel="stylesheet">                                                                                           
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>                                                                                                                                    
+    <script src="<?=get_url('assets/js/jquery-1.10.2.min.js')?>"></script>
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->                                                                                                                       
     <!--[if lt IE 9]>                                                                                                                                                                                       
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>                                                                                                                      
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>                                                                                                                   
+      <script src="<?=get_url('assets/js/html5shiv.js')?>"></script>                                                                                                                      
+      <script src="<?=get_url('assets/js/respond.min.js')?>"></script>                                                                                                                   
     <![endif]-->                                                                                                                                                                                            
   </head> 
